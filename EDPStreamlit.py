30a655c1b5a2eaee64941b1f0c4d79f28b10206c
Samy Mekkaoui <samymekkaoui77@gmail.com>
2023-12-11 23:00:22 +0100

    EDP Pricing v1

---
+++ EDPStreamlit.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 11 22:31:22 2023
+
+@author: samym
+"""
+
+from mainEDP import *
+import streamlit as st
+
+
+
+
+
+st.title("Application pour le calcul d'options par méthode des différences finies")
+
+def user_input():
+    option=st.selectbox("Quelle Option voulez-vous pricer par la méthode de Monte-Carlo ",('Put Européen','Call Européen'))
+    return option
+
+
+
+
+
+df=user_input()
+
+st.set_option('deprecation.showPyplotGlobalUse', False)
+st.header("Vous avez choisi de prendre  le " + str(df))
+
+
+if df=="Put Européen":
+    SpotPrice=st.sidebar.number_input("Choississez le spot",value= 100)
+    Strike=st.sidebar.number_input("Choississez le strike K",value= 100)
+    Maturité=st.sidebar.number_input("Choississez la maturité T",value= 1)
+    Volatilité=st.sidebar.number_input("Choississez σ",value= 0.1)
+    TauxSansRisque=st.sidebar.number_input("Choississez r",value= 0.02)
+    Schéma=st.selectbox("Choissisez le schéma de discrétisation : ",("EE","IE","CN"))
+    data={'''SpotPrice''': SpotPrice,
+         '''Strike''':Strike,
+         '''Maturité''':Maturité,
+         '''Volatilité''':Volatilité,
+         '''Taux Sans Risque''':TauxSansRisque
+         }
+    Parametres=pd.DataFrame(data,index=["Caractéristique de l'option"])
+    st.subheader("Voici les caractéristiques de l'option : ")
+    st.write(Parametres)   
+    
+    Option1=OptionPricer(TauxSansRisque,Volatilité,Strike,Maturité)
+    N=st.number_input("Choissisez le nombre N",value=100)
+    I=st.number_input("Choississez le nombre I",value=20)
+    Smin=st.number_input("Choississez Smin",value=0)
+    Smax=st.number_input("Choississez Smax",value=200)
+    
+    Option1.Setting(Smin=Smin,Smax=Smax,N=N,I=I)
+    Option1.computeScheme(SCHEME=Schéma)
+    
+    
+    st.pyplot(Option1.plot_u())
+    st.write(Option1.BlackScholes(SpotPrice))
+
+
+
---
+++ mainEDP.py
@@ -0,0 +1,123 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 11 22:31:39 2023
+
+@author: samym
+"""
+
+import numpy as np
+import numpy.linalg as lng  # linear algebra
+import matplotlib.pyplot as plt
+import scipy.stats as stats  # pour la fonction de repartition de la loi normale
+import pandas as pd  # pour un meilleur affichage des tables
+
+
+class OptionPricer():
+    def __init__(self,r=0.1,sigma=0.2,K=100,T=1):
+        self.r=r
+        self.sigma=sigma
+        self.K=K
+        self.T=T
+
+    def Setting(self,Smin,Smax,N,I):
+        self.Smax=Smax
+        self.Smin=Smin
+        self.I=I
+        self.N=N
+        self.h=(self.Smax-self.Smin)/(self.I)
+        self.dt=self.T/(self.N)
+        self.S=np.arange(self.Smin,self.Smax+self.h,self.h)
+        self.S_squared = np.array([x**2 for x in self.S])
+        self.grille=np.arange(0,self.T+self.dt,self.dt)
+        alpha=pow(self.sigma,2)/(2*pow(self.h,2))*self.S_squared
+        beta=self.r/(2*self.h)*self.S
+        self.U = np.zeros((self.I,1))
+        for i in range(self.I):
+            #remplissage de U0
+            self.U[i] = self.u0(self.S[i])
+
+        self.A=np.zeros((self.I,self.I))
+        for i in range(1,len(self.A)+1):
+            self.A[i-1][i-1]=2*alpha[i]+self.r
+            if i>1:
+                self.A[i-1][i-2]=-alpha[i]+beta[i]
+
+            if i<self.I:
+                self.A[i-1][i]=-alpha[i]-beta[i]
+
+        return self.A
+
+
+
+    def u0(self,s):
+        return max(self.K-s,0)
+
+    def uleft(self,t):
+        return (self.K*np.exp(-self.r*t)-self.Smin)
+
+    def uright(self,t):
+        return 0
+
+    def q(self,t):
+        y=np.zeros((self.I,1))
+        alpha1=pow(self.sigma,2)/(2*pow(self.h,2))*self.S_squared[1]
+        beta1=(self.r*self.S[1])/(2*self.h)
+        y[0]=(-alpha1+beta1)*self.uleft(t)
+        alphaI=pow(self.sigma,2)/(2*pow(self.h,2))*self.S_squared[self.I]
+        betaI=(self.r*self.S[self.I])/(2*self.h)
+        y[self.I-1]=(-alphaI-betaI)*self.uright(t)
+        return y
+
+
+
+    def computeScheme(self, SCHEME="EE"):
+        self.SCHEME = SCHEME
+
+        if self.SCHEME == "EE":
+            for n in range(0, self.N):
+                self.U = self.U - self.dt * (self.A @ self.U + self.q(self.grille[n]))
+
+        if self.SCHEME == "IE":
+            for n in range(0, self.N):
+                self.U = lng.solve(np.identity(self.I ) + self.dt * self.A,
+                                   -self.dt * self.q(self.grille[n]) + self.U)
+
+        if self.SCHEME == "CN":
+            theta = 0.5
+            for n in range(0, self.N):
+                F = -(theta * self.q(self.grille[n]) + (1 - theta) * self.q(self.grille[n]))
+                part1 = self.U - (1 - theta) * self.dt * (self.A @ self.U)
+                self.U = lng.solve(np.identity(self.I) + theta * self.dt * self.A, self.dt * F + part1)
+
+    def plot_u(self):
+        #print("Pricing par le schema ", self.SCHEME, " avec I= {} ,  N= {}".format(self.I, self.N))
+        # Utilisation de self.I pour s'assurer que les indices sont valides
+        plt.xlabel('Prix du sous-jacent (S)')
+        plt.ylabel('Prix du Put')
+        plt.title("Pricing du Put Européen par le schema {} avec T ={}, I= {},  N= {}".format(self.SCHEME,self.T,self.I,self.N))
+        plt.grid(True)
+        plt.legend()
+        plt.plot(self.S[:self.I], self.U[:self.I])
+        plt.plot(self.S[:self.I + 1], [self.u0(self.S[i]) for i in range(self.I + 1)])
+        plt.show()
+
+
+    def amplification(self):
+        B = np.identity(self.I + 1) - self.dt * self.A
+        return {"matrice": B, "norme": lng.norm(B, np.inf)}
+
+    def CFLnumber(self):
+        mu = self.dt * ((self.sigma * self.Smax / self.h) ** 2)
+        return mu
+
+    def P_Interpolate(self, sbar):
+        i = np.argmax(self.S >= sbar)
+        ubar = ((self.S[i] - sbar) * self.U[i - 1] + (sbar - self.S[i - 1]) * self.U[i]) / self.h
+        return ubar
+
+    def BlackScholes(self, sbar):
+        dplus = (np.log(sbar / self.K) + self.r + 0.5 * self.T * (self.sigma ** 2)) / (self.sigma * np.sqrt(self.T))
+        dmoins = (np.log(sbar / self.K) + self.r - 0.5 * self.T * (self.sigma ** 2)) / (self.sigma * np.sqrt(self.T))
+        vbar = np.exp(-self.r * self.T) * self.K * stats.norm.cdf(-dmoins) - sbar * stats.norm.cdf(-dplus)
+        return vbar
+
